296aaf02c666d5de8cad1f7183cdc3c4d0e84dc2
P-Joel-Prakash <135586760+P-Joel-Prakash@users.noreply.github.com>
2023-06-08 18:41:45 +0530

    Add files via upload

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,13 @@
+// Exporting an array
+export let myArray = ["camel", 265, true, "5.6"];
+
+// Exporting an object
+export let bulb = {
+  watts: 10,
+  type: "LED",
+};
+
+// Exporting a function
+export function multiplyByFour(num) {
+  return num * 4;
+}
